c8cd92dbc475ad8747ebcbf17303c8f97d0f9dcc
Au-en_Triever <108985566+JapsSwain@users.noreply.github.com>
2025-01-11 22:05:42 +0800

    Update students.controller.ts

--- src/students/students.controller.ts
+++ src/students/students.controller.ts
@@ -1,13 +1,26 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Patch, Param,  Get, Body, NotFoundException } from '@nestjs/common';
 import { StudentsService } from './students.service';
 import { CreateStudentDto } from './dto/create-student.dto';
+import { Student } from './entities/student.entity';
 
 @Controller('students')
 export class StudentsController {
   constructor(private readonly studentsService: StudentsService) {}
 
   @Post()
-  create(@Body() createStudentDto: CreateStudentDto) {
+  async create(@Body() createStudentDto: CreateStudentDto): Promise<Student> {
     return this.studentsService.create(createStudentDto);
   }
+
+  @Get()
+  async findAll(): Promise<Student[]> {
+    return this.studentsService.findAll();
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: number, @Body() updateStudentDto: CreateStudentDto) {
+    return this.studentsService.update(id, updateStudentDto);
+  }
+
+
 }
