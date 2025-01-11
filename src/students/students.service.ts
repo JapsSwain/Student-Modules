0d91cbad77085582facee0a8e54617fe23617f59
Au-en_Triever <108985566+JapsSwain@users.noreply.github.com>
2025-01-11 21:59:47 +0800

    Update students.service.ts

--- src/students/students.service.ts
+++ src/students/students.service.ts
@@ -1,18 +1,42 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Student } from './entities/student.entity';
 import { CreateStudentDto } from './dto/create-student.dto';
+import { UpdateStudentDto } from './dto/update-student.dto';
 
 @Injectable()
-export class StudentsService {
+export class StudentService {   
   constructor(
     @InjectRepository(Student)
-    private readonly studentRepository: Repository<Student>,
+    private studentRepository: Repository<Student>,
   ) {}
 
-  create(createStudentDto: CreateStudentDto): Promise<Student> {
+  async create(createStudentDto: CreateStudentDto): Promise<Student> {
     const student = this.studentRepository.create(createStudentDto);
-    return this.studentRepository.save(student);
+    return await this.studentRepository.save(student);
+  }
+
+  async findAll(): Promise<Student[]> {
+    return await this.studentRepository.find();
+  }
+
+  async findOne(id: number): Promise<Student> {
+    const student = await this.studentRepository.findOne({ where: { id } });
+    if (!student) {
+      throw new NotFoundException(`Student with ID ${id} not found`);
+    }
+    return student;
+  }
+
+  async update(id: number, updateStudentDto: UpdateStudentDto): Promise<Student> {
+    const student = await this.findOne(id);
+    Object.assign(student, updateStudentDto);
+    return await this.studentRepository.save(student);
+  }
+
+  async remove(id: number): Promise<void> {
+    const student = await this.findOne(id);
+    await this.studentRepository.remove(student);
   }
 }
