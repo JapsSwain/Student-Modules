2416f671ddaf965a2d08c6fca91a75e4825240af
Au-en_Triever <108985566+JapsSwain@users.noreply.github.com>
2025-01-11 21:59:08 +0800

    Update students.controller.ts

--- src/students/students.controller.ts
+++ src/students/students.controller.ts
@@ -1,13 +1,36 @@
-import { Controller, Post, Body } from '@nestjs/common';
-import { StudentsService } from './students.service';
+import { Controller, Get, Post, Body, Patch, Param, Delete, HttpCode, HttpStatus } from '@nestjs/common';
+import { StudentService } from './student.service';
 import { CreateStudentDto } from './dto/create-student.dto';
+import { UpdateStudentDto } from './dto/update-student.dto';
 
 @Controller('students')
-export class StudentsController {
-  constructor(private readonly studentsService: StudentsService) {}
+export class StudentController {
+  constructor(private readonly studentService: StudentService) {}
 
   @Post()
+  @HttpCode(HttpStatus.CREATED)
   create(@Body() createStudentDto: CreateStudentDto) {
-    return this.studentsService.create(createStudentDto);
+    return this.studentService.create(createStudentDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.studentService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.studentService.findOne(+id);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateStudentDto: UpdateStudentDto) {
+    return this.studentService.update(+id, updateStudentDto);
+  }
+
+  @Delete(':id')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  remove(@Param('id') id: string) {
+    return this.studentService.remove(+id);
   }
 }
